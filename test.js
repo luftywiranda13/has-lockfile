6bacbc716fccbd316fa1976e280d9bc4b4a137ae
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-17 20:47:51 +0700

    feat(API): `.lockfiles()` (#11)
    BREAKING CHANGE:
    
    * `haslockfile()` returns `boolean`
    * `haslockfile.lockfiles()` returns `Array` of lockfiles

--- test.js
+++ test.js
@@ -4,29 +4,35 @@ const hasLockfile = require('./');
 
 describe('output', () => {
   test('no lockfiles', () => {
-    expect(hasLockfile()).toEqual([]);
-    expect(hasLockfile('fixtures')).toEqual([]);
+    expect(hasLockfile()).toBe(false);
+    expect(hasLockfile.lockfiles()).toEqual([]);
   });
 
   test('package-lock.json', () => {
-    expect(hasLockfile('fixtures/bar')).toEqual(['package-lock.json']);
+    expect(hasLockfile('fixtures/bar')).toBe(true);
+    expect(hasLockfile.lockfiles('fixtures/bar')).toEqual([
+      'package-lock.json',
+    ]);
   });
 
   test('yarn.lock', () => {
-    expect(hasLockfile('fixtures/baz')).toEqual(['yarn.lock']);
+    expect(hasLockfile('fixtures/baz')).toBe(true);
+    expect(hasLockfile.lockfiles('fixtures/baz')).toEqual(['yarn.lock']);
   });
 
   test('npm-shrinkwrap.json', () => {
-    expect(hasLockfile('fixtures/foo')).toEqual(['npm-shrinkwrap.json']);
+    expect(hasLockfile('fixtures/foo')).toBe(true);
+    expect(hasLockfile.lockfiles('fixtures/foo')).toEqual([
+      'npm-shrinkwrap.json',
+    ]);
   });
 
   test('all lockfiles', () => {
-    const expected = expect.arrayContaining([
+    expect(hasLockfile('fixtures/qux')).toBe(true);
+    expect(hasLockfile.lockfiles('fixtures/qux')).toEqual([
       'package-lock.json',
       'yarn.lock',
       'npm-shrinkwrap.json',
     ]);
-
-    expect(hasLockfile('fixtures/qux')).toEqual(expected);
   });
 });
