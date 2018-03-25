1e1b1c7f8dbf2f96b75f1d04abdb4229170942b1
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-03-25 20:43:51 +0700

    chore(package): update xo to version 0.20.3 (#16)
    * chore(package): update xo to version 0.20.3
    
    Closes #14
    
    * refactor: adapt changes to the latest `xo`

--- test.js
+++ test.js
@@ -1,38 +1,34 @@
 'use strict';
 
-const hasLockfile = require('./');
+const hasLockfile = require('.');
 
-describe('output', () => {
-  test('no lockfiles', () => {
-    expect(hasLockfile()).toBe(false);
-    expect(hasLockfile.lockfiles()).toEqual([]);
-  });
+test('no lockfiles', () => {
+  expect(hasLockfile()).toBe(false);
+  expect(hasLockfile.lockfiles()).toEqual([]);
+});
 
-  test('package-lock.json', () => {
-    expect(hasLockfile('fixtures/bar')).toBe(true);
-    expect(hasLockfile.lockfiles('fixtures/bar')).toEqual([
-      'package-lock.json',
-    ]);
-  });
+test('package-lock.json', () => {
+  expect(hasLockfile('fixtures/bar')).toBe(true);
+  expect(hasLockfile.lockfiles('fixtures/bar')).toEqual(['package-lock.json']);
+});
 
-  test('yarn.lock', () => {
-    expect(hasLockfile('fixtures/baz')).toBe(true);
-    expect(hasLockfile.lockfiles('fixtures/baz')).toEqual(['yarn.lock']);
-  });
+test('yarn.lock', () => {
+  expect(hasLockfile('fixtures/baz')).toBe(true);
+  expect(hasLockfile.lockfiles('fixtures/baz')).toEqual(['yarn.lock']);
+});
 
-  test('npm-shrinkwrap.json', () => {
-    expect(hasLockfile('fixtures/foo')).toBe(true);
-    expect(hasLockfile.lockfiles('fixtures/foo')).toEqual([
-      'npm-shrinkwrap.json',
-    ]);
-  });
+test('npm-shrinkwrap.json', () => {
+  expect(hasLockfile('fixtures/foo')).toBe(true);
+  expect(hasLockfile.lockfiles('fixtures/foo')).toEqual([
+    'npm-shrinkwrap.json',
+  ]);
+});
 
-  test('all lockfiles', () => {
-    expect(hasLockfile('fixtures/qux')).toBe(true);
-    expect(hasLockfile.lockfiles('fixtures/qux')).toEqual([
-      'package-lock.json',
-      'yarn.lock',
-      'npm-shrinkwrap.json',
-    ]);
-  });
+test('all lockfiles', () => {
+  expect(hasLockfile('fixtures/qux')).toBe(true);
+  expect(hasLockfile.lockfiles('fixtures/qux')).toEqual([
+    'package-lock.json',
+    'yarn.lock',
+    'npm-shrinkwrap.json',
+  ]);
 });
